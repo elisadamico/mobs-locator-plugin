80782c08046a14e75c2b82c63248c0c0536cdedb
Elisa D'Amico <127250270+elisadamico@users.noreply.github.com>
2025-07-09 23:55:15 +0100

    Update MOBsLocatorPluginTest.java

--- src/test/java/com/mobslocator/MOBsLocatorPluginTest.java
+++ src/test/java/com/mobslocator/MOBsLocatorPluginTest.java
@@ -1,13 +1,14 @@
-package com.modslocator;
+package com.mobslocator;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import com.mobslocator.MobsLocatorPlugin;
 
 public class MOBsLocatorPluginTest
 {
-	public static void main(String[] args) throws Exception
-	{
-		ExternalPluginManager.loadBuiltin(modslocator.class);
-		RuneLite.main(args);
-	}
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(MobsLocatorPlugin.class);
+        RuneLite.main(args);
+    }
 }
